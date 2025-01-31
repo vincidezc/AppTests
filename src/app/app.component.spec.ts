a8b84bee9782827536c65772824b51de9a14878f
mb demo <test@demo.com>
2025-01-31 17:30:52 +0000

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'final-project' title`, () => {
+  it(`should have the 'QATrainer!' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('final-project');
+    expect(app.title).toEqual('QATrainer!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, final-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Thanks, QATrainer!');
   });
 });
